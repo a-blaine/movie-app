3aac9b0680df0e085893c41aeace789c6c7ee9c7
a-blaine <ai.blaine4@protonmail.com>
2023-06-06 12:12:09 +0100

    Created search, results and movies components

---
+++ src/Results.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Movies from "./Movies";
+
+export default function Results() {
+  return <Movies />;
+}
--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Results from "./Results";
 
 export default function Search() {
   return (
     <div className="Search">
-      <h1>Movie App</h1>
+      <h1>Movieland</h1>
+      <form>
+        <input
+          type="search"
+          placeholder="Enter a movie"
+          autoFocus="off"
+          autoComplete="off"
+        />
+      </form>
+      <Results />
     </div>
   );
 }
