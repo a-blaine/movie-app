a5e00421f4298c54621fd56b3bd50bae523d01c3
a-blaine <ai.blaine4@protonmail.com>
2023-06-07 17:15:47 +0100

    Added styling to and refactored movie cards

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,24 +1,23 @@
 import React from "react";
+import "./MovieCard.css";
 
 export default function MovieCard({ movie }) {
   return (
     <div className="MovieCard">
-      <div className="d-none">
+      <div className="year">
         <p>{movie.Year}</p>
       </div>
-      <div>
-        <img
-          src={
-            movie.Poster !== "N/A"
-              ? movie.Poster
-              : "https://via.placeholder.com/400"
-          }
-          alt={movie.Title}
-          className="img-fluid"
-        />
-      </div>
-      <div>
-        <span className="text-capitalize">{movie.Type}</span>
+      <img
+        src={
+          movie.Poster !== "N/A"
+            ? movie.Poster
+            : "https://via.placeholder.com/400"
+        }
+        alt={movie.Title}
+        className="img-fluid"
+      />
+      <div className="card-footer">
+        <span className="text-uppercase">{movie.Type}</span>
         <h3>{movie.Title}</h3>
       </div>
     </div>
